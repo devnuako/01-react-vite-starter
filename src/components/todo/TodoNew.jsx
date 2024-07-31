7510fce95bc502bd6926246e418ced039492a680
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-07-31 11:00:39 +0700

    37: Manage data with state - useState Hook

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,33 @@
-const TodoNew = (props) => {
-    console.log(">>> check point: ", props)
-    const { addNewTodo } = props;
+import { useState } from "react";
 
-    addNewTodo("testalert")
+const TodoNew = (props) => {
+    // console.log(">>> check point: ", props)
+    // const { addNewTodo } = props;
+    // addNewTodo("testalert") //fire
+
+    //useState hook (getter/setter)
+    const [valueInput, setValueInput] = useState("test1")
+
+    const handleClick = () => {
+        console.log(">>> check valueInput: ", valueInput)
+    }
+
+    const handleOnChange = (name) => {
+        setValueInput(name)
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" 
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
+            <div>
+                My text input is = {valueInput}
+            </div>
         </div>
     )
 }
