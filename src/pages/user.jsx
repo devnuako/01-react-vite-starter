f4d31c1df2dfd90971d15c4fd898c866df158198
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-08-05 16:09:56 +0700

    67: Use react to manage user form

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -3,7 +3,6 @@ import UserTable from "../components/user/user.table";
 
 const UserPage = () => {
     return (
-        // <div>User page</div>
         <div style={{ padding: "20px" }}>
             <UserForm />
             <UserTable />
