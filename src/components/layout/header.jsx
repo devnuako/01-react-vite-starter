d3a2e5344fa9d936aea79e3ca168fec76cd85374
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-08-05 17:11:56 +0700

    76: Design Modal Update User

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,8 +1,40 @@
 // import { Link } from 'react-router-dom';
 import { Link, NavLink } from 'react-router-dom';
-import './header.css';
+// import './header.css';
+
+import { Menu } from 'antd';
+import { UsergroupAddOutlined, HomeOutlined, AuditOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
 
 const Header = () => {
+    //anchor
+    const [current, setCurrent] = useState('');
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link>,
+            key: 'home',
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/users"}>Users</Link>,
+            key: 'users',
+            icon: <UsergroupAddOutlined />
+        },
+        {
+            label: <Link to={"/books"}>Books</Link>,
+            key: 'books',
+            icon: <AuditOutlined />,
+
+        },
+
+    ];
+    
     return (
         <ul>
             {/* <li><a className="active" href="/">Home</a></li>
@@ -13,11 +45,12 @@ const Header = () => {
             <li><Link to="/users">Users</Link></li>
             <li><Link to="/products">Products</Link></li> */}
             
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/users">Users</NavLink></li>
-            <li><NavLink to="/books">Books</NavLink></li>
-
-
+            <Menu
+                onClick={onClick}
+                selectedKeys={[current]}
+                mode="horizontal"
+                items={items}
+            />
         </ul>
     )
 }
--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,5 +1,9 @@
 import { Table } from 'antd';
 
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import UpdateUserModal from './update.user.modal';
+
+
 const UserTable = (props) => {
     const { dataUsers } = props;
 
@@ -7,6 +11,12 @@ const UserTable = (props) => {
         {
             title: 'Id',
             dataIndex: '_id',
+            render: (_, record) => {
+                return (
+                    <a href='#'>{record._id}</a>
+                )
+            }
+
         },
         {
             title: 'Full Name',
@@ -15,15 +25,29 @@ const UserTable = (props) => {
         {
             title: 'Email',
             dataIndex: 'email',
-        }
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <div style={{ display: "flex", gap: "20px" }}>
+                    <EditOutlined style={{ cursor: "pointer", color: "orange" }} />
+                    <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                </div>
+            ),
+        },
+
     ];
 
     return (
-        <Table
-            columns={columns}
-            dataSource={dataUsers}
-            rowKey={"_id"}
-        />
+        <>
+            <Table
+                columns={columns}
+                dataSource={dataUsers}
+                rowKey={"_id"}
+            />
+            <UpdateUserModal />
+        </>
     )
 }
 
