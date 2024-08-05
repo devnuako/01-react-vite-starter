12f96bd21c44125379359e96f4a82c646368bdaf
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-08-05 16:44:15 +0700

    73: useEffect Hook: Update user list once

--- src/main.jsx
+++ src/main.jsx
@@ -51,8 +51,9 @@ const router = createBrowserRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <RouterProvider router={router} />
+  // </React.StrictMode>,
+  <RouterProvider router={router} />
+
 )
