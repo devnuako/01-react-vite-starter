ee0e03e6e5f94c3aafd521ceea57b521dbdb71d6
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-08-06 10:31:55 +0700

    87: Pagination for user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -9,7 +9,12 @@ import { deleteUserAPI } from '../../services/api.service';
 
 
 const UserTable = (props) => {
-    const { dataUsers, loadUser } = props;
+    const { dataUsers, loadUser,
+
+        current, pageSize, total,
+        setCurrent, setPageSize
+    } = props;
+
 
     const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
 
@@ -20,6 +25,15 @@ const UserTable = (props) => {
 
 
     const columns = [
+        {
+            title: "STT",
+            render: (_, record, index) => {
+                console.log(">>> check index: ", index)
+                return (
+                    <>{(index + 1) + (current - 1) * pageSize}</>
+                )
+            }
+        },
         {
             title: 'Id',
             dataIndex: '_id',
@@ -92,6 +106,25 @@ const UserTable = (props) => {
         }
     }
 
+    const onChange = (pagination, filters, sorter, extra) => {
+    // setCurrent, setPageSize
+        //nếu thay đổi trang : current
+        if (pagination && pagination.current) {
+            if (+pagination.current !== +current) {
+                setCurrent(+pagination.current) //"5" => 5
+            }
+        }
+
+        //nếu thay đổi tổng số phần tử : pageSize
+        if (pagination && pagination.pageSize) {
+            if (+pagination.pageSize !== +pageSize) {
+                setPageSize(+pagination.pageSize) //"5" => 5
+            }
+        }
+
+        console.log(">>> check ", { pagination, filters, sorter, extra })
+    };
+
 
     return (
         <>
@@ -99,6 +132,17 @@ const UserTable = (props) => {
                 columns={columns}
                 dataSource={dataUsers}
                 rowKey={"_id"}
+                pagination={
+                    {
+                        current: current,
+                        pageSize: pageSize,
+                        showSizeChanger: true,
+                        total: total,
+                        showTotal: (total, range) => { return (<div> {range[0]}-{range[1]} trên {total} rows</div>) }
+                    }
+                }
+                onChange={onChange}
+
             />
 
             <UpdateUserModal
